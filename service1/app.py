19d2f3eb4ec93089673eaaf4be14d286c8b3ea52
Hannah Lister-Sims <hlistersims@gmail.com>
2020-11-26 18:03:47 +0000

    building on service1 app

--- service1/app.py
+++ service1/app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
 
-    upletters_request = requests.get('http://service2:5001/upper')
-    
+    #service2
+    uppercase_request = requests.get('http://service2:5001/upper')
+    uppercase = upletters_request.json()['uppercase']
 
+    lowercase_request = requests.get('http://service2:5001/lower', json={"uppercase": uppercase})
+    lowercase = lowercase_request.json()['lowercase']
 
-    return render_template('index')
+    #service3
+    six_digit_request = requests.get('http://service3:5002/six')
+    six_digit = six_digit_request.json()['six_digit']
+
+    special_num = requests.get('http://service3:/special')
+
+    #service4
+    prize1 =
+    prize2 =
+    prize3 =
+
+    return render_template('index', uppercase=uppercase, lowercase=lowercase, six_digit=six_digit, special_num=special_num)
 
 
 
