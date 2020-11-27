3bf838da52c9fcc8abd7607d03d165266f2e13e7
Hannah Lister-Sims <hlistersims@gmail.com>
2020-11-27 14:59:39 +0000

    serivce3 app done

---
+++ service3/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, Response, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/number', methods=['GET'])
+def get_number():
+
+    num = ["13579", "24680", "48582", "09672", "34167"]
+    number = random.choices(num)
+
+    return Response(number, mimetype="text/plain")
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0', port=5002)
+
+print(number)
