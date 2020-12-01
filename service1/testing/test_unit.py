77e9d91ff2dcaf230656af5f7efe950489cb86f9
Hannah Lister-Sims <hlistersims@gmail.com>
2020-12-01 15:46:12 +0000

    start of testing with service1

---
+++ service1/testing/test_unit.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from service1.app import app
+
+class TestBase( TestCase ) :
+    def create_app(self):
+        return app
+
+class TestResponse( TestBase ) :
+
+    #test for get and post request
+    def test_ticket_shows(self):
+        with patch( "requests.get" ) as g:
+            with patch( "requests.post" ) as p:
+                g.return_value.text = "ABC13579"
+                p.return_value.text = "Nothing, please try again"
+
+                response = self.client.get(url_for("index"))
+                self.assertEqual( response.status_code, 200 )
